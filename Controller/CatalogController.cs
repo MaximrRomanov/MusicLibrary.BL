c28c47ce82e603c1ccbaafdf2f59deadcf7b8eeb
MaximrRomanov <maxprolife@icloud.com>
2022-03-02 20:39:46 +0300

    Add project files.

---
+++ Controller/CatalogController.cs
@@ -0,0 +1,53 @@
+﻿using MusicLibrary.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace MusicLibrary.BL.Controller
+{
+    public class CatalogController
+    {
+        public List<Catalog>  Catalogs { get; }
+        public Catalog CurrentCatalog { get; set; }
+        
+        public CatalogController(string catalogName)
+        {
+            Catalogs = GetDataFromXml();
+            CurrentCatalog = Catalogs.SingleOrDefault();
+            // проверка по имени каталога 
+
+            if(CurrentCatalog == null)
+            {
+                // создание нового каталога 
+                SaveDataInXml();
+            }
+        }
+        private void SaveDataInXml()
+        {
+            XDocument xml = new XDocument(new XElement("Library",
+                new XElement("catalog",
+                    new XAttribute("name", $"{CurrentCatalog.CatalogName}"),
+                    new XElement("author", $"{CurrentCatalog.Author}"),
+                    new XElement("albom name", $"{CurrentCatalog.AlbomName}"),
+                    new XElement("genre", $"{CurrentCatalog.Genre}"),
+                    new XElement("country", $"{CurrentCatalog.Country}"))));
+            xml.Save("library.xml");
+        }
+        private static List<Catalog> GetDataFromXml()
+        {
+           
+            return new List<Catalog>();
+        }
+        public void SetNewCatalog(string catalogName, string author, string albomName, DateTime date, string country, DateTime releaseDate)
+        {
+            CurrentCatalog = new Catalog(catalogName, author, albomName, date, country, releaseDate);
+            CurrentCatalog.CatalogName = catalogName;
+            CurrentCatalog.Author = author;
+            CurrentCatalog.AlbomName = albomName;
+            CurrentCatalog.Date = date;
+            CurrentCatalog.Country = country;
+            CurrentCatalog.ReleaseDate = releaseDate;
+        }
+    }
+}
---
+++ Model/Catalog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace MusicLibrary.BL.Model
+{
+    public class Catalog
+    {
+        public string CatalogName { get; set; }
+        public string Author { get; set; }
+        public string AlbomName { get; set; }
+        public string Genre { get; set; }
+        public DateTime Date { get; set; }
+        public string Country { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Catalog(string catalogName, string author, string albomName, DateTime date, string country, DateTime releaseDate)
+        {
+            if (string.IsNullOrWhiteSpace(catalogName))
+            {
+                throw new ArgumentNullException("Имя каталога не может быть пустым или равно null!", nameof(catalogName));
+            }
+            
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                throw new ArgumentNullException("Имя автора не может быть пустым или равно null!", nameof(author));
+            }
+            if (string.IsNullOrWhiteSpace(albomName))
+            {
+                throw new ArgumentNullException("Название альбома не может быть пустым или равно null!", nameof(albomName));
+            }
+            if (string.IsNullOrWhiteSpace(country))
+            {
+                throw new ArgumentNullException("Название страны не может быть пустым или равно null!", nameof(country));
+            }
+            CatalogName = catalogName;
+            Author = author;
+            AlbomName = albomName;
+            Country = country;
+            Date = date;
+            ReleaseDate = releaseDate;
+        }
+        public Catalog(string catalogName)
+        {
+            if (string.IsNullOrWhiteSpace(catalogName))
+            {
+                throw new ArgumentNullException("Имя каталога не может быть пустым или равно null!", nameof(catalogName));
+            }
+            CatalogName = catalogName;
+        }
+        public override string ToString()
+        {
+            return CatalogName;
+        }
+    }
+}
